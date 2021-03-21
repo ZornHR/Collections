291f413bde71f5bf241d092560be89e93826dfe2
ZornHR <ratrednecks@gmail.com>
2021-03-21 13:16:15 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(final String[] args) {
+        MyCollection<String> myCollection = new MyCollection<>();
+        myCollection.add("1");
+        myCollection.add("3");
+        myCollection.add("3");
+    }
+}
+
---
+++ src/com/company/MyCollection.java
@@ -0,0 +1,135 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyCollection<E> implements Collection<E> {
+
+    private int size;
+
+    private Object[] elementData = new Object[10];
+
+    @Override
+    public String toString() {
+        return "MyCollection{" +
+                "size=" + size +
+                ", elementData=" + Arrays.toString(elementData) +
+                '}';
+    }
+
+    private class MyIterator<T> implements Iterator<T> {
+
+        int cursor = 0;
+
+        @Override
+        public boolean hasNext() {
+            return cursor < size;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public T next() {
+            if (cursor >= size) {
+                throw new NoSuchElementException();
+            }
+            return (T) elementData[cursor++];
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("remove");
+        }
+    }
+
+    @Override
+    public boolean add(E e) {
+        if (size == elementData.length) {
+            elementData = Arrays.copyOf(elementData, (int) (size * 1.5f));
+        }
+        elementData[size++] = e;
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new MyIterator<>();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (Object x : elementData) {
+            if (o != null) {
+                if (o.equals(x)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(elementData, size);
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (o != null) {
+                if (o.equals(elementData[i])) {
+                    Object[] arrRem = new Object[elementData.length];
+                    // https://javarush.ru/help/9543
+                    System.arraycopy(elementData, 0, arrRem, 0, i);
+                    System.arraycopy(elementData, i + 1, arrRem, i, elementData.length - 1 - i);
+                    elementData = arrRem;
+                    size--;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+        elementData = new Object[10];
+    }
+}
